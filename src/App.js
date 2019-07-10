438594626e14dd2e28973cb92b341187a3c3ed84
Nikolas Zec <nikolas.zec@gmail.com>
2019-07-10 11:04:15 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Layout from '../src/components/Layout/Layout'
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <Layout>X</Layout>
+      )
+  }
+  
+}
+
+export default App;
